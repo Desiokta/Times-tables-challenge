18d24df8d9a5d26a9424741179958901b2f5b940
desiokta <desiokt10@gmail.com>
2021-10-26 15:55:17 +0700

    Times Tables

--- app/src/main/java/com/example/timetables/MainActivity.java
+++ app/src/main/java/com/example/timetables/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.timetables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
